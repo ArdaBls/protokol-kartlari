5affd081c0af8f6957462e9822708cfd247b210a
Arda Bilasa <bilasaarda@gmail.com>
2026-09-15 01:50:04 +0300

    Revert "feat: Blackjack'e Godot masası eklendi (?godot=1, DENEYSEL)"
    This reverts commit f4a9e3883911c7b6f569c034a435e08f2516a7a0.

--- docs/js/oyun-blackjack-DWcGCiKH.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["js/blackjack-oyun-DpiS6o68.js","js/main-v4-BGl9BFB5.js","js/toast-C9dk_WzR.js","js/db-mode-F_jprkvV.js","assets/main-v4-Ba6gl62N.css"])))=>i.map(i=>d[i]);
-import{n as a}from"./main-v4-BGl9BFB5.js";a(()=>import("./blackjack-oyun-DpiS6o68.js").then(a=>a.initBlackjack()),__vite__mapDeps([0,1,2,3,4]));
